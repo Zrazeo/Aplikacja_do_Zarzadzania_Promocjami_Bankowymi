6b854e85728955ba0b8e11f3a0a815bf66c424e1
Krystian Osmelak <osmelakkrystian@gmail.com>
2022-02-26 19:53:54 +0100

    jakies zmiany

--- apk/lib/screens/karencja_screen.dart
+++ apk/lib/screens/karencja_screen.dart
@@ -1,7 +1,5 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'dart:convert';
-import 'package:flutter/services.dart';
 
 class KarencjaScreen extends StatefulWidget {
   @override
@@ -9,46 +7,114 @@ class KarencjaScreen extends StatefulWidget {
 }
 
 class _KarencjaScreen extends State<KarencjaScreen> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text('Karencja'),
-        ),
-        body: Container(
-          child: Center(
-            // Use future builder and DefaultAssetBundle to load the local JSON file
-            child: FutureBuilder(
-                future: DefaultAssetBundle.of(context)
-                    .loadString('assets/karencja.json'),
-                builder: (context, snapshot) {
-                  // Decode the JSON
-                  var new_data = json.decode(snapshot.data.toString());
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Karencja'),
+      ),
+      body: Container(
+        child: Center(
+          // Use future builder and DefaultAssetBundle to load the local JSON file
+          child: FutureBuilder(
+              future: DefaultAssetBundle.of(context)
+                  .loadString('assets/karencja.json'),
+              builder: (context, snapshot) {
+                // Decode the JSON
+                var new_data = json.decode(snapshot.data.toString());
 
-                  return ListView.builder(
-                    // Build the ListView
-                    itemBuilder: (BuildContext context, int index) {
-                      return Card(
-                        child: Padding(
-                          padding: EdgeInsets.all(20.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            children: <Widget>[
-                              Text("Nazwa Banku: " + new_data[index]['name']),
-                              SizedBox(
-                                height: 10,
-                              ),
-                              Text("Data zamknięcia konta: " + new_data[index]['data']),
-                            ],
-                          ),
+                return ListView.builder(
+                  // Build the ListView
+                  itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                      child: Padding(
+                        padding: EdgeInsets.all(20.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                            Text("Nazwa Banku: " + new_data[index]['name']),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text("Data zamknięcia konta: " +
+                                new_data[index]['data']),
+                          ],
                         ),
-                      );
-                    },
-                    itemCount: new_data == null ? 0 : new_data.length,
-                  );
-                }),
-          ),
-        ));
+                      ),
+                    );
+                  },
+                  itemCount: new_data == null ? 0 : new_data.length,
+                );
+              }),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                content: Stack(
+                  overflow: Overflow.visible,
+                  children: <Widget>[
+                    Positioned(
+                      right: -40.0,
+                      top: -40.0,
+                      child: InkResponse(
+                        onTap: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: CircleAvatar(
+                          child: Icon(Icons.close),
+                          backgroundColor: Colors.blue,
+                        ),
+                      ),
+                    ),
+                    Form(
+                      key: _formKey,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              decoration:
+                                  InputDecoration(labelText: 'Nazwa banku'),
+                            ),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                  labelText: 'Data zamknięcia konta'),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: RaisedButton(
+                              child: Text("Dodaj"),
+                              onPressed: () {
+                                if (_formKey.currentState.validate()) {
+                                  _formKey.currentState.save();
+                                }
+                              },
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+        label: const Text('Dodaj bank'),
+        icon: const Icon(Icons.add),
+        backgroundColor: Colors.blue,
+      ),
+    );
   }
 }
